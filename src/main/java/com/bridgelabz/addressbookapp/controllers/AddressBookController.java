9b5afffbbec204d429cf1fad10fb1842db4777ad
Aditya Verma <aaadarths2357@gmail.com>
2020-12-14 21:45:26 +0530

    Added: updated method names

--- src/main/java/com/bridgelabz/addressbookapp/controllers/AddressBookController.java
+++ src/main/java/com/bridgelabz/addressbookapp/controllers/AddressBookController.java
@@ -14,29 +14,29 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping(value = "/addressbookservice")
 public class AddressBookController {
-	
+
 	@GetMapping(value = { "", "/", "/get" })
-	public ResponseEntity<String> getEmployeePayrollData() {
+	public ResponseEntity<String> getAddressBookContactData() {
 		return new ResponseEntity<String>("Get Call Success", HttpStatus.OK);
 	}
 
 	@GetMapping("/get/{id}")
-	public ResponseEntity<String> getEmployeePayrollData(@PathVariable("id") int id) {
+	public ResponseEntity<String> getAddressBookContactData(@PathVariable("id") int id) {
 		return new ResponseEntity<String>("Get Call Success for id: " + id, HttpStatus.OK);
 	}
 
 	@PostMapping("/create")
-	public ResponseEntity<String> addEmployeePayrollData(@RequestBody String name) {
+	public ResponseEntity<String> addAddressBookContactData(@RequestBody String name) {
 		return new ResponseEntity<String>("Created Address Book Contact Data for: " + name, HttpStatus.OK);
 	}
 
 	@PutMapping("/update/{id}")
-	public ResponseEntity<String> updateEmployeePayrollData(@PathVariable("id") int id) {
+	public ResponseEntity<String> updateAddressBookContactData(@PathVariable("id") int id) {
 		return new ResponseEntity<String>("Updated Address Book Contact Data for id: " + id, HttpStatus.OK);
 	}
 
 	@DeleteMapping("/delete/{id}")
-	public ResponseEntity<String> deleteEmployeePayrollData(@PathVariable("id") int id) {
+	public ResponseEntity<String> deleteAddressBookContactData(@PathVariable("id") int id) {
 		return new ResponseEntity<String>("Delete Call Success for id: " + id, HttpStatus.OK);
 	}
 }
