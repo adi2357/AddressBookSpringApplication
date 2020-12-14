814333d9f327f78ebc3b670ec65d8ab2079ddad7
Aditya Verma <aaadarths2357@gmail.com>
2020-12-15 00:31:51 +0530

    Added: repository layer to enable the services layer to interact with the database using mysql db

---
+++ src/main/java/com/bridgelabz/addressbookapp/repository/AddressBookRepository.java
@@ -0,0 +1,11 @@
+package com.bridgelabz.addressbookapp.repository;
+
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+import com.bridgelabz.addressbookapp.model.AddressBookData;
+
+@Repository
+public interface AddressBookRepository extends CrudRepository<AddressBookData, Integer> {
+
+}
